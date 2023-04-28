0510db58c5593e14e978835edd354fa6260f7687
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-04-28 22:39:49 +0100

    Done with number methods

---
+++ numbers/methods.rb
@@ -0,0 +1,27 @@
+int = 10
+float = 10.6
+neg = - 6
+
+puts int
+puts float
+puts int + float
+puts int - float
+puts int * float
+puts float / int
+puts 2 ** 3
+puts 7 % 2
+
+# Print num in string
+puts "Hello world "+int.to_s
+
+# Let's use methods
+
+puts neg.abs()
+puts float.round()
+puts float.ceil()
+puts float.floor()
+
+# Math Class
+
+puts Math.sqrt(144)
+puts Math.log(100)
