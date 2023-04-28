9098d6401400e778ce599fdb97ac9d18c05adf2e
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-04-28 21:51:51 +0100

    Done with print

---
+++ hello-advanced.rb
@@ -0,0 +1,6 @@
+# User Puts - This adds a string to a different line
+puts 'Hello world, I used put here.'
+
+# Normal Print - Prints on same line
+print 'Hello world, Femi started writing Ruby.'
+print ' I feel good.'
