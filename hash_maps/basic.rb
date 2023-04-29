684710a6c3924ed60ea9d0d288e3aa47ebb82dd9
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-04-29 15:36:11 +0100

    Done with hash maps

---
+++ hash_maps/basic.rb
@@ -0,0 +1,14 @@
+myself = {
+    "name" => "Femi",
+    "age" => 21,
+    "major" => "Computer Science",
+    "location" => "Kaduna"
+}
+
+puts myself
+puts myself["age"]
+myself["age"] = 22
+puts myself["age"]
+
+# Methods
+puts myself.length()
