a43cd7ac6f0e075d6623641891b70cf5ff25d0ec
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-05-05 10:41:23 +0100

    Done with for loops

---
+++ loops/for.rb
@@ -0,0 +1,17 @@
+friends = ['Samson', "Samuel", "Daniel", "Ope", "Precious"]
+
+for friend in friends
+    puts friend
+end
+puts "-----------------------"
+friends.each do |friend|
+    puts friend
+end
+puts "-----------------------"
+for num in 1..10
+    puts num
+end
+puts "-----------------------"
+6.times do |index|
+    puts index
+end
